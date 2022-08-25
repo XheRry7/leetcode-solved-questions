dfd1f4178e5bef3955592735f125a1fab3ae7621
shehryar ahmed <shehryarahmed1744@gmail.com>
2022-08-25 20:16:28 +0500

    Time: 129 ms (40.00%), Space: 43.8 MB (52.91%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    const sqrtx = Math.sqrt(x);
+    const decimal = parseInt(sqrtx);
+    return decimal;
+    
+};
