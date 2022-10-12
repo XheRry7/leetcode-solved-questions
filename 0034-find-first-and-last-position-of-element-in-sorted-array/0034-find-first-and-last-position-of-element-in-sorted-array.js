056f7805cdfe018e5f3b936bb4e748598296c223
shehryar ahmed <shehryarahmed1744@gmail.com>
2022-10-13 01:15:07 +0500

    Time: 105 ms (33.53%), Space: 42.6 MB (47.76%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -5,7 +5,6 @@
  */
 var searchRange = function(nums, target) {
     if(nums.includes(target)){
-          console.log(nums.indexOf(target) ,nums.lastIndexOf(target));
         return [nums.indexOf(target) ,nums.lastIndexOf(target)]
     }
     return [-1,-1]
