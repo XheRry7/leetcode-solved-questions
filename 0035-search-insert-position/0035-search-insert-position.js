86d1b2dc096458cebf33761c744752e6134483f1
shehryar ahmed <shehryarahmed1744@gmail.com>
2022-10-13 01:05:15 +0500

    Time: 92 ms (55.92%), Space: 43.6 MB (6.97%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,11 +4,7 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-  
- 
      nums.push(target);
     let y =  nums.sort((a,b)=>a-b);
    return y.indexOf(target)
-    
-    
 };
