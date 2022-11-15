47d8e6c4e3cb05a9d7a8390e7b5bf880f13c02c0
shehryar ahmed <shehryarahmed1744@gmail.com>
2022-11-15 23:58:13 +0500

    Time: 79 ms (79.23%), Space: 41.8 MB (74.10%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    
+    let a = 0, b = 0;
+    while( b < t.length && a < s.length) {
+        if(s[a] === t[b]) {
+            a++;
+        }
+        b++;        
+    }
+    return a === s.length ? true: false;
+    
+};
