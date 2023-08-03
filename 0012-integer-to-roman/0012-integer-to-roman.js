aa98424c17f5a90271803b9e5419b76f24aeb577
shehryar ahmed <shehryarahmed1744@gmail.com>
2023-08-03 11:00:16 -0700

    Time: 182 ms (8.34%), Space: 53.4 MB (5.51%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,96 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+  let newNumber = num.toString().split("");
+  let finalNum = [];
+  function Convertor(givenNumber) {
+    // block for four numbers Working fine
+    if (givenNumber.length == 4) {
+      for (let i = 0; i < givenNumber[0]; i++) {
+        finalNum.push("M");
+      }
+      newNumber.shift();
+    }
+
+    // block for three number block working fine
+    if (givenNumber.length == 3) {
+      if (givenNumber[0] < 4) {
+        for (let i = 0; i < givenNumber[0]; i++) {
+          finalNum.push("C");
+        }
+      }
+      if (givenNumber[0] == 4) {
+        finalNum.push("CD");
+      }
+      if (givenNumber[0] >= 5 && givenNumber[0] < 9) {
+        finalNum.push("D");
+        let number = givenNumber[0] - 4;
+        for (let i = 1; i < number; i++) {
+          finalNum.push("C");
+        }
+      }
+      if (givenNumber[0] == 9) {
+        finalNum.push("CM");
+      }
+      newNumber.shift();
+    }
+
+    // block for two numbers
+
+    if (givenNumber.length == 2) {
+      if (givenNumber[0] < 4) {
+        for (let i = 0; i < givenNumber[0]; i++) {
+          finalNum.push("X");
+        }
+      }
+      if (givenNumber[0] == 4) {
+        finalNum.push("XL");
+      }
+      if (givenNumber[0] >= 5 && givenNumber[0] < 9) {
+        finalNum.push("L");
+        let number = givenNumber[0] - 4;
+        for (let i = 1; i < number; i++) {
+          finalNum.push("X");
+        }
+      }
+
+      if (givenNumber[0] == 9) {
+        finalNum.push("XC");
+      }
+
+      newNumber.shift();
+    }
+
+    // block for a single number
+    if (givenNumber.length == 1) {
+      if (givenNumber[0] < 4) {
+        for (let i = 0; i < givenNumber[0]; i++) {
+          finalNum.push("I");
+        }
+      }
+      if (givenNumber[0] == 4) {
+        finalNum.push("IV");
+      }
+      if (givenNumber[0] >= 5 && givenNumber[0] < 9) {
+        finalNum.push("V");
+        let number = givenNumber[0] - 4;
+        for (let i = 1; i < number; i++) {
+          finalNum.push("I");
+        }
+      }
+      if (givenNumber[0] == 9) {
+        finalNum.push("IX");
+      }
+
+      newNumber.shift();
+    }
+  }
+  if (newNumber.length > 0) {
+    let res = Convertor(newNumber);
+  }
+
+  console.log("finalNum::::", finalNum.join(""));
+  return finalNum.join("");
+};
