073064182d7965a76a7b774c9f190d02492ad88a
shehryar ahmed <shehryarahmed1744@gmail.com>
2023-10-20 04:41:45 +0500

    Time: 507 ms (78.97%), Space: 0B (100.00%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,5 +1,8 @@
-SELECT
-  Score,
-  (SELECT count(distinct Score) FROM Scores WHERE Score >= s.Score) as  'Rank'
-FROM Scores s
-ORDER BY Score desc
+#SELECT
+#  Score,
+#  (SELECT count(distinct Score) FROM Scores WHERE Score >= s.Score) as  'Rank'
+#FROM Scores s
+#ORDER BY Score desc
+
+SELECT score, dense_rank() OVER(ORDER BY score DESC) as "rank"
+FROM Scores
