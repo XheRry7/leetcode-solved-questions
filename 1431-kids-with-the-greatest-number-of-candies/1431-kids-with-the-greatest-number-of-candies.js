83c29fa503086223915ecb23ed48c9db227570d2
shehryar ahmed <shehryarahmed1744@gmail.com>
2024-10-16 21:02:09 +0400

    Time: 56 ms (68.55%), Space: 50.6 MB (40.54%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -4,18 +4,22 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
-    let candiesCopy = [...candies];
-    let sortedArray = candiesCopy.sort((a,b)=> a-b);
-    let greatestNum = sortedArray[candies.length - 1];
+//     let candiesCopy = [...candies];
+//     let sortedArray = candiesCopy.sort((a,b)=> a-b);
+//     let greatestNum = sortedArray[candies.length - 1];
     
-    let arr = [];
-    for(let i = 0 ; i < candies.length; i++){
-        if(candies[i] + extraCandies >= greatestNum){
-            arr.push(true);
-        }
-        else{
-            arr.push(false)
-        }
-    }
-    return arr;
+//     let arr = [];
+//     for(let i = 0 ; i < candies.length; i++){
+//         if(candies[i] + extraCandies >= greatestNum){
+//             arr.push(true);
+//         }
+//         else{
+//             arr.push(false)
+//         }
+//     }
+//     return arr;
+
+    let max = Math.max(...candies);
+    return candies.map((kidCandyAmount)=> kidCandyAmount + extraCandies >= max);
+
 };
